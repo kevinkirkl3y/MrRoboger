2ace08f93bef2fbe682a6b65f3f42ee2e3836c93
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-30 10:59:09 -0700

    starting the loop to search through our numRange to find targeted numbers and replace them.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,19 @@ const beep = "Beep!"
 const boop = "Boop!"
 const wontYa = "Won't you be my neighbor?"
 
-// creates range of numbers for the numBumbler to loop through. 
+// creates range of numbers for the numBumbler to loop throug
 let numBumbler = function(input) {
   let numRange = [];
-  for (let i = 0; i <= input; i++)
-  numRange.push(i.toString());
+  for (let i = 0; i <= input; i++){
+    numRange.push(i.toString());
+  };
+  //loops through numRange to identify numbers, because we want 3 to override 2 and 1 respectively we want it to resort to doing that first. Because we are trying to transform an array into a new array and we are using strings a map loop is our best option. 
+  let numBumblerS2 = numRange.map(function(numRange))
+  }
   console.log(numRange);
-}
+
+};
+
 
 
 
