885c141777e75bb8ccd3c95d1da8a8ba6ac802a5
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-30 11:49:27 -0700

    successfully printing results out to page.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,11 @@ const beep = "Beep!"
 const boop = "Boop!"
 const wontYa = "Won't you be my neighbor?"
 
-// creates range of numbers for the numBumbler to loop throug
 let numBumbler = function(input) {
   let numRange = [];
   for (let i = 0; i <= input; i++){
     numRange.push(i.toString());
   };
-  //loops through numRange to identify numbers, because we want 3 to override 2 and 1 respectively we want it to resort to doing that first. Because we are trying to transform an array into a new array and we are using strings a map loop is our best option. 
   let numBumblerS2 = numRange.map(function(numRange) {
     if (numRange.includes("3")){
       return numRange = wontYa;
@@ -22,8 +20,7 @@ let numBumbler = function(input) {
       return numRange;
     };
   });
-  console.log(numBumblerS2);
-
+  return numBumblerS2
 };
 
 
@@ -36,7 +33,8 @@ $(document).ready(function(){
     event.preventDefault();
     let input = parseInt($("input#original").val());
     let output = numBumbler(input)
-  })
+    $("#outputRange").append("<li>" + output + "</li>")
+  });
   
   
   
